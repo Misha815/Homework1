22c3d768d3d6247c66c1adc3013e7b4bc51cde86
Mikhael Stepnov <71971590+Misha815@users.noreply.github.com>
2022-10-14 09:09:14 +0300

    git init, quest 1

---
+++ quest1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("EX1");
+Console.WriteLine("Введите два значения и нажмите Enter:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+    if (a > b)
+   {
+         Console.WriteLine("Максимальное число "+ a);
+    }   
+        else if (b >a)
+   {
+        Console.WriteLine("Максимальное число " + b);
+    }
+
+    if (a < b)
+    {
+        Console.WriteLine($"Минамальное число {a}");
+    } else if (b < a)
+    {
+        Console.WriteLine($"Минимальное число {b}");    
+    }
