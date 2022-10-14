f5068e383a6cda9e11a3aec256d62baf4ffccf61
Mikhael Stepnov <71971590+Misha815@users.noreply.github.com>
2022-10-14 11:14:20 +0300

    quest4

---
+++ quest4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Какое будет число?:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int current = 0;
+int i;
+
+while (current < a) {
+if (current%2 == 0){
+    Console.WriteLine($"{current}");
+}
+current++;}
+
